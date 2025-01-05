4aa8d07667f7ee5bf0d73d77322a342bc99d2d99
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2025-01-05 19:27:26 +0530

    Basic navigation and toast notifications addeed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,11 @@ import {Navigate,Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import EditorPage from './pages/EditorPage'
 import ErrorPage from './pages/ErrorPage'
+import {Toaster} from 'react-hot-toast' 
 function App() {
   return (
     <>
+    <Toaster position='top-center'></Toaster>
     <Routes>
       <Route path='/' element={<Navigate to="/home"/>}/>
       <Route path='/home' element={<HomePage/>}/>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'; // Import v4 for generating unique IDs
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 import Logo from '../assets/Logo.png';
 
 function HomePage() {
   const [roomId, setRoomId] = useState('');
   const [userName, setUserName] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = () => {
     if (roomId.trim() === '' || userName.trim() === '') {
-      alert('Please enter both Room ID and Username');
+      toast.error('Please enter both Room ID and Username');
       return;
     }
-    // Logic to handle login goes here (e.g., redirect to a room or validate inputs)
-    console.log('Room ID:', roomId, 'Username:', userName);
+    if (roomId.trim().length < 5) {
+      toast.error('Room ID must be at least 5 characters long');
+      return;
+    }
+    navigate(`/editor/${roomId}`,{state:userName})
+    toast.success('Room Created  Successfully');
+  };
+
+  const handleCreateRoom = () => {
+    const newRoomId = uuidv4(); 
+    setRoomId(newRoomId); 
+    toast.success("Room ID generated Successfully ")
+
+    setTimeout(() => navigate(`/editor/${roomId}`,{state:userName}), 1000);
   };
 
   return (
@@ -38,9 +54,14 @@ function HomePage() {
           onClick={handleLogin}
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-300"
         >
-          Join Room 
+          Join Room
         </button>
-        <p>Don't have a Room ID ? Create New Room </p>
+        <button
+          onClick={handleCreateRoom}
+          className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-green-300"
+        >
+          Create New Room
+        </button>
       </div>
     </div>
   );
