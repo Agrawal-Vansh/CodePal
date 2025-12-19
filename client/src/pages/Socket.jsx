c443cbbb2416d7b4b12a3b4be4882abf9325b9fa
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2025-12-19 19:33:16 +0530

    allow more polling for render to make connection

--- client/src/pages/Socket.jsx
+++ client/src/pages/Socket.jsx
@@ -5,10 +5,10 @@ let socketInstance = null;
 const createSocket = () => {
   if (!socketInstance) {
     socketInstance = io(import.meta.env.VITE_SERVER_URL, {
-      transports: ["websocket"],
       reconnection: true,
       reconnectionAttempts: 5,
-      timeout: 10000,
+      timeout: 20000, // Render needs more time
+      withCredentials: true,
     });
   }
   return socketInstance;
